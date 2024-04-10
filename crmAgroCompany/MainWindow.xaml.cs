148185f1d20858a942cc669d9071c770df1dbf51
KRAEVSKIY BOGDAN <118996991+GiVennHeM@users.noreply.github.com>
2024-04-10 23:22:21 +0300

    Add project files.

---
+++ crmAgroCompany/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MaterialDesignThemes.Wpf;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace crmAgroCompany;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        homeBorder.Visibility = Visibility.Visible;
+        clientsBorder.Visibility = Visibility.Hidden;
+        ClientsButton.IsEnabled = true;
+        if(HomeButton.IsEnabled)
+           HomeButton.Background = new SolidColorBrush(Colors.Red);
+    }
+
+    private void ClientsButton_Click(object sender, RoutedEventArgs e)
+    {
+        homeBorder.Visibility = Visibility.Hidden;
+        clientsBorder.Visibility = Visibility.Visible;
+    }
+
+    private void HomeButton_Click(object sender, RoutedEventArgs e)
+    {
+        homeBorder.Visibility = Visibility.Visible;
+        clientsBorder.Visibility = Visibility.Hidden;
+    }
+
+    private void addClientButton_Click(object sender, RoutedEventArgs e)
+    {
+        AddNewCustomer addNewCustomer = new AddNewCustomer();
+        addNewCustomer.ShowDialog();
+    }
+}
---
+++ crmAgroCompany/dbContext/dbContext.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Windows;
+
+namespace crmAgroCompany
+{
+    class dbContext
+    {
+        private string connectionString = @"Data Source=DESKTOP-4UM6HKE;Initial Catalog=NEW_AgroproCRM;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
+
+        public bool Login(string username, string password)
+        {
+            bool isAuthenticated = false;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT Password FROM LoggerUser WHERE Login = @Login";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Login", username);
+
+                        string hashedPassword = (string)command.ExecuteScalar();
+
+                        if (hashedPassword != null && HashHelper.VerifyHash(password, hashedPassword))
+                        {
+                            isAuthenticated = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+                MessageBox.Show($"Error executing login request: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            return isAuthenticated;
+        }
+
+
+        public bool SignUp(string name, string surname, string numberofphone, int position, string login, string password, string profileImageSource)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO LoggerUser (Username, Password, Login, PhoneNumber, ProfilePicture, Surname) " +
+                                   "VALUES (@Username, @Password, @Login, @PhoneNumber, @ProfilePicture, @Surname)";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Username", name);
+                        command.Parameters.AddWithValue("@Surname", surname);
+                        command.Parameters.AddWithValue("@PhoneNumber", numberofphone);
+                        command.Parameters.AddWithValue("@Position", position);
+                        command.Parameters.AddWithValue("@Login", login);
+                        command.Parameters.AddWithValue("@Password", password);
+                        command.Parameters.AddWithValue("@ProfilePicture", profileImageSource);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        return rowsAffected > 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error executing sign up request: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+    }
+}
